48988649f64939d7735b10482700395b11fd5084
PedroLucas003 <pedrolucasaraujo201733@gmail.com>
2025-05-31 09:02:49 -0300

    foi3

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
-});
+}); 
 
 // Hash da senha antes de salvar
 userSchema.pre('save', async function(next) {
